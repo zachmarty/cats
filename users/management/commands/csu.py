059e7bd7a47951b20591e187f260f8312b6db732
Pavel <pashapasha-2013@mail.ru>
2024-10-02 16:34:54 +0300

    readme added

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,23 +1,19 @@
 from typing import Any
 from django.core.management import BaseCommand
 from users.models import User
-from rest_framework.exceptions import NotFound
 
 class Command(BaseCommand):
     """Команда для создания суперпользователя"""
     def handle(self, *args: Any, **options: Any) -> str | None:
-        try:
-            admin = User.objects.get(email="admin@mail.ru")
-            admin.delete()
-        except:
-            pass
-        admin = User.objects.create(
-            email="admin@mail.ru",
-            first_name="admin",
-            last_name="admin",
-            is_staff=True,
-            is_superuser=True,
-            is_active = True,
-        )
-        admin.set_password("admin")
-        admin.save()
+        admin = User.objects.filter(email="admin@mail.ru")
+        if not admin.exists():
+            admin = User.objects.create(
+                email="admin@mail.ru",
+                first_name="admin",
+                last_name="admin",
+                is_staff=True,
+                is_superuser=True,
+                is_active = True,
+            )
+            admin.set_password("admin")
+            admin.save()
